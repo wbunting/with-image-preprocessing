67527166c7ca09579b332b3e8b07aa285bf9e1b6
Will <bunting.will@gmail.com>
2020-03-15 15:28:15 +0900

    removed node

--- node_modules/node-vibrant/lib/browser.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-var vibrant_1 = __importDefault(require("./vibrant"));
-var browser_1 = __importDefault(require("./image/browser"));
-vibrant_1.default.DefaultOpts.ImageClass = browser_1.default;
-module.exports = vibrant_1.default;
-//# sourceMappingURL=browser.js.map
